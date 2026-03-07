7055c70ef7da97f20b583f8fbe3bed39349088b4
Adarsh <adarshdhawale267@gmail.com>
2026-03-07 12:43:22 +0530

    config.ts changes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // 1. Enable Standalone Mode
+  // This bundles only the necessary files for production into a single folder.
+  output: 'standalone',
+
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" },
